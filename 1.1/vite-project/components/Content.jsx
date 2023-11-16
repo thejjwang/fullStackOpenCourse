8d2c6f73170e47794850ce2a15016c35d673f9ff
thejjwang <jonathanjacobwang@gmail.com>
2023-11-15 21:59:47 -0800

    refactored the parts into objects

--- 1.1/vite-project/components/Content.jsx
+++ 1.1/vite-project/components/Content.jsx
@@ -5,9 +5,9 @@ import React from "react";
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
       {/* <p>
         {props.part1} {props.exercises1}
       </p>
--- 1.1/vite-project/components/Part.jsx
+++ 1.1/vite-project/components/Part.jsx
@@ -4,7 +4,7 @@ const Part = (props) => {
   return (
     <div>
         <p>
-            {props.part} {props.exercises}
+            {props.part.name} {props.part.exercises}
         </p>
     </div>
   )
