22c336cc3ecd740d8e139321a01359b5e52ef2bb
thejjwang <jonathanjacobwang@gmail.com>
2023-11-16 11:01:22 -0800

    1.4 done refactored using array of obj

--- 1.1/vite-project/components/Content.jsx
+++ 1.1/vite-project/components/Content.jsx
@@ -2,12 +2,17 @@ import Part from "./Part";
 
 import React from "react";
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <ul>
+        {parts.map((part, index) => (
+          <li key={index}>
+            {part.name} - {part.exercises} exercises
+          </li>
+        ))}
+      </ul>
+      
       {/* <p>
         {props.part1} {props.exercises1}
       </p>
--- 1.1/vite-project/components/Total.jsx
+++ 1.1/vite-project/components/Total.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Total = (props) => {
+const Total = ({parts}) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
-        <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+      <p>Total exercises: {totalExercises}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Total
+export default Total;
