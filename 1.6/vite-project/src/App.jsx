b317ef2cf2e87194f1d89e58dfe718dfa86d7d14
thejjwang <jonathanjacobwang@gmail.com>
2023-11-17 11:48:39 -0800

    refactored 1.8 done

--- 1.6/vite-project/src/App.jsx
+++ 1.6/vite-project/src/App.jsx
@@ -1,23 +1,12 @@
 import { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
   const totalFeedback = good + neutral + bad;
   const averageScore = (good - bad) / totalFeedback;
   const positivePercentage = (good / totalFeedback) * 100;
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <div>
-        <button onClick={() => setGood((prev) => prev + 1)}>good</button>
-        <button onClick={() => setNeutral((prev) => prev + 1)}>neutral</button>
-        <button onClick={() => setBad((prev) => prev + 1)}>bad</button>
-      </div>
       <h1>statistics</h1>
       <div>
         <p>good {good}</p>
@@ -27,6 +16,24 @@ const App = () => {
         <p>average {averageScore}</p>
         <p>positive {positivePercentage}</p>
       </div>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGood((prev) => prev + 1)}>good</button>
+        <button onClick={() => setNeutral((prev) => prev + 1)}>neutral</button>
+        <button onClick={() => setBad((prev) => prev + 1)}>bad</button>
+      </div>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
