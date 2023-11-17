374e0604d48e8852c481d6a669be7653753b5a8f
thejjwang <jonathanjacobwang@gmail.com>
2023-11-17 11:25:35 -0800

    1.6 done

---
+++ 1.6/vite-project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGood((prev) => prev + 1)}>good</button>
+        <button onClick={() => setNeutral((prev) => prev + 1)}>neutral</button>
+        <button onClick={() => setBad((prev) => prev + 1)}>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
